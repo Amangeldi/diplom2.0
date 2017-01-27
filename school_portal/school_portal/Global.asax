c390f4637094df6a6c1b929e97d0d6a9d16d0e24
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-27 21:18:25 +0300

    start project

---
+++ school_portal/school_portal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="school_portal.MvcApplication" Language="C#" %>
---
+++ school_portal/school_portal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(school_portal.Startup))]
+namespace school_portal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
