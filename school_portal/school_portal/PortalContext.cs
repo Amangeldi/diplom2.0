c7f41afd6080b520a91ff72aacea81d3917350b6
Amangeldi <Amangeldi.9606@gmail.com>
2017-01-28 21:30:33 +0300

    Add data context

---
+++ school_portal/school_portal/PortalContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace school_portal
+{
+    public class PortalContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Teacher> Teacher { get; set; }
+        public DbSet<Student> Stusent { get; set; }
+        public DbSet<Parent> Parent { get; set; }
+        public DbSet<Groupp> Groupp { get; set; }
+    }
+}
